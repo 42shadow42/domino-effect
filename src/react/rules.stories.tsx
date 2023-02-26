a5ce4aa762b9f8720ce99022879b7f665d1fe566
Nathaniel Hunter <42shadow42@gmail.com>
2023-02-25 23:41:27 -0600

    Added async domino splitting documentation

--- src/react/rules.stories.tsx
+++ src/react/rules.stories.tsx
@@ -3,6 +3,8 @@ import { action } from '@storybook/addon-actions'
 import { Component, Fragment, memo } from 'react'
 import { domino, trigger } from '../dominos'
 import { useDomino } from './useDomino'
+import { useAsyncDomino } from './useAsyncDomino'
+import { DominoSuspense } from './suspense'
 export const Trigger: ComponentStory<typeof Component> = () => {
 	const core = trigger('Hello')
 
@@ -153,3 +155,92 @@ export const SplittingDominos: ComponentStory<typeof Component> = () => {
 
 	return <SplittingDominos />
 }
+
+export const SplittingAsyncDominos: ComponentStory<typeof Component> = () => {
+	const core = trigger(
+		Promise.resolve({
+			greeting: 'Hello',
+			target: 'world!',
+		}),
+	)
+
+	const manager = domino(async ({ get, manage }) => {
+		return {
+			values: await get(core),
+			manage: manage(core),
+		}
+	})
+
+	const greeting = domino(async ({ get }) => {
+		return (await get(core)).greeting
+	})
+
+	const target = domino(async ({ get }) => {
+		return (await get(core)).target
+	})
+
+	const GreetingDisplay = () => {
+		const value = useAsyncDomino(greeting)
+		action('greeting rendered')(value)
+		return <Fragment>{value}</Fragment>
+	}
+
+	const TargetDisplay = () => {
+		const value = useAsyncDomino(target)
+		action('target rendered')(value)
+		return <Fragment>{value}</Fragment>
+	}
+
+	const Editor = () => {
+		const value = useAsyncDomino(manager)
+		return (
+			<Fragment>
+				<input
+					type="text"
+					value={value.values.greeting}
+					onChange={(evt) =>
+						value.manage.set(
+							Promise.resolve({
+								...value.values,
+								greeting: evt.target.value,
+							}),
+						)
+					}
+				/>
+				<input
+					type="text"
+					value={value.values.target}
+					onChange={(evt) =>
+						value.manage.set(
+							Promise.resolve({
+								...value.values,
+								target: evt.target.value,
+							}),
+						)
+					}
+				/>
+			</Fragment>
+		)
+	}
+
+	const SplittingDominosAsync = () => {
+		return (
+			<Fragment>
+				<h4>
+					<DominoSuspense fallback="loading">
+						{GreetingDisplay}
+					</DominoSuspense>
+					<DominoSuspense fallback="loading">
+						{TargetDisplay}
+					</DominoSuspense>
+				</h4>
+				<DominoSuspense fallback="loading">
+					{Editor}
+				</DominoSuspense>
+			</Fragment>
+		)
+	}
+
+	return <SplittingDominosAsync />
+}
+
