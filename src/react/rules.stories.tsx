411c2d749c1399c669a5b4eddce4c8a6f01f8bc1
Nathaniel Hunter <42shadow42@gmail.com>
2023-02-25 23:19:55 -0600

    Added domino splitting documentation

--- src/react/rules.stories.tsx
+++ src/react/rules.stories.tsx
@@ -1,5 +1,6 @@
 import { ComponentStory } from '@storybook/react'
-import { Component, Fragment } from 'react'
+import { action } from '@storybook/addon-actions'
+import { Component, Fragment, memo } from 'react'
 import { domino, trigger } from '../dominos'
 import { useDomino } from './useDomino'
 export const Trigger: ComponentStory<typeof Component> = () => {
@@ -57,12 +58,16 @@ export const SplittingComponents: ComponentStory<typeof Component> = () => {
 		)
 	}
 
-	return (
-		<Fragment>
-			<Domino />
-			<Trigger />
-		</Fragment>
-	)
+	const SplittingComponents = () => {
+		return (
+			<Fragment>
+				<Domino />
+				<Trigger />
+			</Fragment>
+		)
+	}
+
+	return <SplittingComponents />
 }
 
 export const CombiningDominos: ComponentStory<typeof Component> = () => {
@@ -78,7 +83,7 @@ export const CombiningDominos: ComponentStory<typeof Component> = () => {
 		}
 	})
 
-	const Combined = () => {
+	const CombiningDominos = () => {
 		const value = useDomino(combined)
 		return (
 			<Fragment>
@@ -92,7 +97,59 @@ export const CombiningDominos: ComponentStory<typeof Component> = () => {
 		)
 	}
 
-	return (
-		<Combined />
-	)
+	return <CombiningDominos />
+}
+
+export const SplittingDominos: ComponentStory<typeof Component> = () => {
+	const core = trigger({
+		greeting: 'Hello',
+		target: 'world!',
+	})
+
+	const greeting = domino(({ get }) => {
+		return get(core).greeting
+	})
+
+	const target = domino(({ get }) => {
+		return get(core).target
+	})
+
+	const GreetingDisplay = memo(() => {
+		const value = useDomino(greeting)
+		action('greeting rendered')(value)
+		return <Fragment>{value}</Fragment>
+	})
+
+	const TargetDisplay = memo(() => {
+		const value = useDomino(target)
+		action('target rendered')(value)
+		return <Fragment>{value}</Fragment>
+	})
+
+	const SplittingDominos = () => {
+		const [value, setValue] = useDomino(core)
+		return (
+			<Fragment>
+				<h4>
+					<GreetingDisplay /> <TargetDisplay />
+				</h4>
+				<input
+					type="text"
+					value={value.greeting}
+					onChange={(evt) =>
+						setValue({ ...value, greeting: evt.target.value })
+					}
+				/>
+				<input
+					type="text"
+					value={value.target}
+					onChange={(evt) =>
+						setValue({ ...value, target: evt.target.value })
+					}
+				/>
+			</Fragment>
+		)
+	}
+
+	return <SplittingDominos />
 }
