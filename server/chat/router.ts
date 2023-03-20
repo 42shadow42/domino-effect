7ee3d61677db04f878b821e1e4136bd58ee69cd7
Nathaniel Hunter <42shadow42@gmail.com>
2023-03-19 20:40:01 -0500

    ESLint the chat server directory, cleanup those files.

--- server/chat/router.ts
+++ server/chat/router.ts
@@ -6,73 +6,85 @@ const router = express()
 const ews = expressWS(router)
 
 const broadcastMessage = (chatRoom: ChatRoom, message: ChatMessage) => {
-    chatRoom.sockets.forEach((ws) => {
-        ws.send(JSON.stringify({
-            signal: 'message',
-            data: message,
-        }))
-    })
+	chatRoom.sockets.forEach((ws) => {
+		ws.send(
+			JSON.stringify({
+				signal: 'message',
+				data: message,
+			}),
+		)
+	})
 }
 const broadcastJoinEvent = (chatRoom: ChatRoom, user: string) => {
-    chatRoom.sockets.forEach((ws) => {
-        ws.send(JSON.stringify({
-            signal: 'user_join',
-            data: user,
-        }))
-    })
+	chatRoom.sockets.forEach((ws) => {
+		ws.send(
+			JSON.stringify({
+				signal: 'user_join',
+				data: user,
+			}),
+		)
+	})
 }
 const broadcastLeaveEvent = (chatRoom: ChatRoom, user: string) => {
-    chatRoom.sockets.forEach((ws) => {
-        ws.send(JSON.stringify({
-            signal: 'user_leave',
-            data: user,
-        }))
-    })
+	chatRoom.sockets.forEach((ws) => {
+		ws.send(
+			JSON.stringify({
+				signal: 'user_leave',
+				data: user,
+			}),
+		)
+	})
 }
 
 ews.app.ws('/:id', (ws, req) => {
-    const id = req.params.id
-    const user = req.query.user as string
+	const id = req.params.id
+	const user = req.query.user as string
 
-    ws.on('message', (message) => {
-        const chatRoom = CHAT_ROOMS.get(id)!
-        const chatMessage: ChatMessage = { user, message: message.toString() }
-        chatRoom.messages.push(chatMessage)
-        broadcastMessage(chatRoom, chatMessage)
-    })
+	ws.on('message', (message) => {
+		const chatRoom = CHAT_ROOMS.get(id)!
+		const chatMessage: ChatMessage = { user, message: message.toString() }
+		chatRoom.messages.push(chatMessage)
+		broadcastMessage(chatRoom, chatMessage)
+	})
 
-    ws.on('close', () => {
-        const chatRoom = CHAT_ROOMS.get(id)!
-        chatRoom.sockets = chatRoom.sockets.filter((lobbySocket) => lobbySocket !== ws)
-        if (chatRoom.sockets.length === 0) {
-            CHAT_ROOMS.delete(id)
-            return
-        }
-        chatRoom.users = chatRoom.users.filter((lobbyUser) => lobbyUser !== user)
-        const message: ChatMessage = { user, message: "Cya Later!"}
-        chatRoom.messages.push(message)
-        broadcastMessage(chatRoom, message)
-        broadcastLeaveEvent(chatRoom, user)
-    })
+	ws.on('close', () => {
+		const chatRoom = CHAT_ROOMS.get(id)!
+		chatRoom.sockets = chatRoom.sockets.filter(
+			(lobbySocket) => lobbySocket !== ws,
+		)
+		if (chatRoom.sockets.length === 0) {
+			CHAT_ROOMS.delete(id)
+			return
+		}
+		chatRoom.users = chatRoom.users.filter(
+			(lobbyUser) => lobbyUser !== user,
+		)
+		const message: ChatMessage = { user, message: 'Cya Later!' }
+		chatRoom.messages.push(message)
+		broadcastMessage(chatRoom, message)
+		broadcastLeaveEvent(chatRoom, user)
+	})
 
-    if (!CHAT_ROOMS.has(id)) {
-        CHAT_ROOMS.set(id, {
-            messages: [],
-            users: [],
-            sockets: [],
-        })
-    }
-    const chatRoom = CHAT_ROOMS.get(id)!
-    const message: ChatMessage = { user, message: "Hello everyone!" }
-    chatRoom.messages.push(message)
-    chatRoom.users.push(user)
-    broadcastJoinEvent(chatRoom, user)
-    broadcastMessage(chatRoom, message)
-    chatRoom.sockets.push(ws)
-    ws.send(JSON.stringify({
-        signal: 'init',
-        chat: { ...chatRoom, sockets: undefined },
-    }))
+	if (!CHAT_ROOMS.has(id)) {
+		CHAT_ROOMS.set(id, {
+			messages: [],
+			users: [],
+			sockets: [],
+		})
+	}
+	const chatRoom = CHAT_ROOMS.get(id)!
+	const message: ChatMessage = { user, message: 'Hello everyone!' }
+	chatRoom.messages.push(message)
+	chatRoom.users.push(user)
+	broadcastJoinEvent(chatRoom, user)
+	broadcastMessage(chatRoom, message)
+	chatRoom.sockets.push(ws)
+	ws.send(
+		JSON.stringify({
+			signal: 'init',
+			chat: { ...chatRoom, sockets: undefined },
+		}),
+	)
 })
 
 export const ChatRouter = router
